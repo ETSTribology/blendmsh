43c38aaa867a8081317a1fce720533feea2c161d
Antoine Boucher <antoine.bou13@gmail.com>
2024-10-23 12:25:32 -0400

    Update properties.py

--- properties.py
+++ properties.py
@@ -2,72 +2,50 @@ import bpy
 from bpy.props import StringProperty, IntProperty, FloatProperty, EnumProperty, BoolProperty
 
 class BlendmshProperties(bpy.types.PropertyGroup):
+    initialized: BoolProperty(default=False)
 
-        initialized : BoolProperty(default=False)
+    workspace_path: StringProperty(
+        name="Workspace Path",
+        description="Path where mesh results are stored",
+        default='/tmp/',
+        subtype='DIR_PATH'
+    )
 
-        workspace_path : StringProperty(
-                name="",
-                description="Path for the results",
-                default='/tmp/',
-                subtype='DIR_PATH')
+    cl_max: FloatProperty(
+        name="Element Size",
+        default=0.1,
+        min=0.1,
+        max=5.0,
+        precision=3,
+        description="Maximum size of the mesh element."
+    )
 
-        cl_max : FloatProperty(
-                name="",
-                default=0.1,
-                min=0.1,
-                max = 5.0,
-                precision=3,
-                description="Maximum size of the element.")
+    n_physicalgroups: IntProperty(
+        name="Physical Groups",
+        default=0,
+        min=0,
+        description="Number of physical groups."
+    )
 
-        n_physicalgroups : IntProperty(
-                name="Physical groups",
-                default=0,
-                min=0,
-                description="Number of physical groups.")
+    element_order: EnumProperty(
+        name='Element Order',
+        items=[('1', '1', 'First Order'), ('2', '2', 'Second Order')],
+        default='1'
+    )
 
-        element_order : EnumProperty(
-                name='Element order',
-                items=[
-                        ('1', '1', 'First order elements'),
-                        ('2', '2', 'Second order elements'),
-                        ('3', '3', 'Third order elements')],
-                default='1',
-                description='Element order')
+    mesh_dimension: EnumProperty(
+        name='Mesh Dimension',
+        items=[('2', '2D', '2D Mesh'), ('3', '3D', '3D Mesh')],
+        default='3'
+    )
 
-        mesh_dimension : EnumProperty(
-                name='Mesh dimension',
-                items=[
-                        ('1', '1D', '1D mesh'),
-                        ('2', '2D', '2D mesh'),
-                        ('3', '3D', '3D mesh')],
-                default='3',
-                description='Dimension of output mesh')
+    algorithm: EnumProperty(
+        name='Meshing Algorithm',
+        items=[('0', 'Auto', 'Automatic Meshing')],
+        default='0'
+    )
 
-        algorithm : EnumProperty(
-                name='',
-                items=[
-                        ('0', 'auto', 'Automatic'),
-                        ('1', 'meshadapt', 'Meshadapt'),
-                        ('2', 'del2d', 'Delunay2D'),
-                        ('3', 'front2d', 'Front2D'),
-                        ('4', 'delquad', 'Delquad'),
-                        ('5', 'pack', 'Pack'),
-                        ('6', 'initial2d', 'Initial2D'),
-                        ('7', 'del3d', 'Delunay3D'),
-                        ('8', 'front3d', 'Front3D'),
-                        ('9', 'mmg3d', 'MMG3D'),
-                        ('10', 'hxt', 'HXT'),
-                        ('11', 'initial3d', 'Initial3D')],
-
-                default='0',
-                description="Algorithm")
-
-        output_file_format : EnumProperty(
-                name='Output',
-                items=[
-                        ('.msh', '.msh', 'Gmsh (.msh)'),
-                        ('.inp', '.inp', 'Abaqus (.inp)'),
-                        ('.vtk', '.vtk', 'The Visualization Toolkit (.vtk)')],
-                default='.msh',
-                description='Output file format')
-
+    output_file_format: EnumProperty(
+        name='Output Format',
+        items=[('.msh', '.msh', 'Gmsh format'), ('.vtk', '.vtk', 'VTK format')],
+        default='.
