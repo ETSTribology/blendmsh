276fff0bfd3dfec5596db9921c1b2672a2efcd5d
Antoine Boucher <antoine.bou13@gmail.com>
2024-10-23 12:38:29 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,28 +1,3 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-#################################################
-
-# -------------------
-# Blender for Science
-# -------------------
-# Add-on: blendmsh
-# Author: Senthur Raj (Github: imsenthur)
-# Description: Blendmsh is a bridge between Blender 2.80+ and Gmsh, a fast and light 3D finite element mesh generator.
-# https://github.com/blender-for-science/blendmsh
-
-#################################################
-
 bl_info = {
     "name": "Blendmsh",
     "author": "Senthur Raj",
@@ -44,7 +19,9 @@ def register():
     bpy.utils.register_class(BlendmshInstaller)
     bpy.utils.register_class(BlendmshProperties)
     bpy.utils.register_class(BLENDMSH_PT_Panel)
+    
     bpy.types.Scene.blendmsh = bpy.props.PointerProperty(type=BlendmshProperties)
+    
     bpy.utils.register_class(BLENDMSH_OT_Meshinit)
     bpy.utils.register_class(BLENDMSH_OT_Meshproc)
     bpy.utils.register_class(BLENDMSH_OT_Physicalgroups)
