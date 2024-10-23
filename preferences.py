4cdfb9bba25079d65f182d8b490a37679531c9b3
Antoine Boucher <antoine.bou13@gmail.com>
2024-10-23 12:24:25 -0400

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -1,37 +1,31 @@
 import bpy
+import importlib
+from .utils_pip import Pip
 
 class BlendmshPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
     def draw(self, context):
-
-        import importlib
-        from .utils_pip import Pip
-        Pip._ensure_user_site_package()
-
         layout = self.layout
+        Pip._ensure_user_site_package()
 
         if importlib.util.find_spec('gmsh-api') is not None:
             layout.label(text='gmsh-api loaded.', icon='INFO')
         else:
             layout.label(text='Blendmsh requires gmsh-api!', icon='ERROR')
-            row = layout.row()
-            row.operator('blendmsh.installer')
+            layout.operator('blendmsh.installer', text="Install gmsh-api")
+
 
 class BlendmshInstaller(bpy.types.Operator):
     bl_idname = "blendmsh.installer"
     bl_label = "Install gmsh-api"
-    bl_description = ("Install gmsh-api")
+    bl_description = "Install gmsh-api via pip"
 
     def execute(self, context):
         try:
-            from .utils_pip import Pip
-            # Pip.upgrade_pip()
             Pip.install('gmsh-api')
-
             import gmsh_api
-            print(gmsh_api.__version__)
-            self.report({'INFO'}, 'Successfully installed gmsh-api.')
+            self.report({'INFO'}, f'Successfully installed gmsh-api {gmsh_api.__version__}.')
         except ModuleNotFoundError:
-            self.report({'ERROR'}, 'Could not install gmsh-api, Kindly install it manually.')
+            self.report({'ERROR'}, 'Could not install gmsh-api, please install manually.')
         return {'FINISHED'}
